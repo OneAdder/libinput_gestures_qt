7f8ab42358a17ee24f6aeb73bab8b3875e0ba982
OneAdder <mikivo@list.ru>
2019-04-24 09:30:10 +0300

    various new features

--- main.py
+++ main.py
@@ -129,13 +129,19 @@ class GesturesApp(QtWidgets.QMainWindow, main_window.Ui_MainWindow):
         fingers = []
         shortcuts = []
         buttons = []
+        actions = []
         for line in conf:
             if line.startswith('gesture'):
                 splitted = line.replace('\t', ' ').split()
                 action = '{} {} {}'.format(splitted[0], splitted[1], splitted[2])
                 gestures.append(QtWidgets.QLabel(reversed_mapping['{} {} {}'.format(splitted[0], splitted[1], splitted[2])]))
                 fingers.append(QtWidgets.QLabel(splitted[3]))
-                shortcuts.append(QtWidgets.QLabel(splitted[6]))
+                if splitted[4] == 'xdotool' and splitted[5] == 'key':
+                    actions.append(QtWidgets.QLabel('shortcut'))
+                    shortcuts.append(QtWidgets.QLabel(splitted[6]))
+                else:
+                    actions.append(QtWidgets.QLabel('command'))
+                    shortcuts.append(QtWidgets.QLabel(' '.join(splitted[4:])))
                 buttons.append(action)
         self.layout = self.verticalLayout
 
@@ -151,14 +157,17 @@ class GesturesApp(QtWidgets.QMainWindow, main_window.Ui_MainWindow):
         for i, label in enumerate(fingers):
             flay.addWidget(label, i, 1)
 
-        for i, label in enumerate(shortcuts):
+        for i, label in enumerate(actions):
             flay.addWidget(label, i, 2)
+
+        for i, label in enumerate(shortcuts):
+            flay.addWidget(label, i, 3)
 
         for i, button in enumerate(buttons):
             deleteButton = QtWidgets.QPushButton("Delete")
             deleteButton.setAccessibleName(button)
             deleteButton.clicked.connect(self.delete_entry)
-            flay.addWidget(deleteButton, i, 3)
+            flay.addWidget(deleteButton, i, 4)
 
         self.layout.addWidget(self.area)
 
@@ -188,13 +197,33 @@ class EditGestures(QtWidgets.QWidget, edit_window.Ui_Form):
         self.action = ''
         self.fingers = 0
         self.shortcut = ''
-
+        
         self.fingersLine.setMinimum(2)
+
+        self.draw_shortcut()
+        self.shortcut_command.activated[str].connect(self.shortcut_or_command)
         
         self.actionMenu.activated[str].connect(self.action_chosen)
         self.fingersLine.valueChanged[int].connect(self.fingers_chosen)
-        self.keyboardLine.textChanged[str].connect(self.shortcut_chosen)
         self.saveButton.clicked.connect(self.save_changes)
+
+    def shortcut_or_command(self, text):
+        if text == 'Keyboard Shortcut':
+            self.draw_shortcut()
+        else:
+            self.draw_command()
+
+    def draw_shortcut(self):
+        self.actionType.setText('Keyboard Shortcut')
+        self.keyboardLine = QtWidgets.QKeySequenceEdit()
+        self.gridLayout.addWidget(self.keyboardLine, 4, 2)
+        self.keyboardLine.keySequenceChanged.connect(self.shortcut_chosen)
+        
+    def draw_command(self):
+        self.actionType.setText('Command')
+        self.commandLine = QtWidgets.QLineEdit()
+        self.gridLayout.addWidget(self.commandLine, 4, 2)
+        self.commandLine.textChanged[str].connect(self.command_chosen)
 
     def action_chosen(self, text):
         self.action = actions_mapping[text]
@@ -203,6 +232,9 @@ class EditGestures(QtWidgets.QWidget, edit_window.Ui_Form):
         self.fingers = value
 
     def shortcut_chosen(self, text):
+        self.shortcut = 'xdotool key ' + text.toString()
+
+    def command_chosen(self, text):
         self.shortcut = text
 
     def save_changes(self):
@@ -212,11 +244,11 @@ class EditGestures(QtWidgets.QWidget, edit_window.Ui_Form):
             for line in conf:
                 if not line.startswith(self.action):
                     new_conf.append(line)
-            new_conf.append('{}\t{} xdotool key {}\n'.format(self.action, str(self.fingers), self.shortcut))
+            new_conf.append('{}\t{} {}\n'.format(self.action, str(self.fingers), self.shortcut))
             write_config(new_conf)
             self.actionMenu.setCurrentIndex(0)
             self.fingersLine.setValue(0)
-            self.keyboardLine.setText('')
+            #self.keyboardLine.setText('')
             QtWidgets.QMessageBox.about(self, "Success", "Cofiguration successfully edited.")
             self.close()
         else:
